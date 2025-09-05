4f002aad26b502309790d4ea4ee0933beaff78ca
Uday461 <udaybanavath963@gmail.com>
2025-09-06 00:27:22 +0530

    Added Unit Converter UI Layer

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -8,48 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      //  VStack {
-            Form {
-                Text("Hello")
-                Text("Hello")
-                Text("Hello")
-                Text("Hello")
-                
-                Section {
-                    Text("Hello")
-                    Text("Hello")
-                }
-            }
-            
-            Form {
-                Text("Hello")
-                Text("Hello")
-                Text("Hello")
-                Text("Hello")
-            }
-        }
-     //   .padding()
-  //  }
-}
-
-struct NavigationView: View {
+    let temperatures = ["Celsius", "Kelvin", "Farenheit"]
+    @State private var temperature = "Celsius"
+    @State private var outputTemp = "Celsius"
+    @State private var inputTemp = 0.0
+    @State private var output = 0.0
+    @FocusState private var tempIsFocused: Bool
+    
+    private var numberFormatter: NumberFormatter  {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                Color(.systemGray6)
-                    .frame(height: 1000)
-//                Text("Uday")
-//                
-//                Spacer()
-//                Spacer()
-//                
-//                Text("Kiran")
+            Form {
+                Section {
+                    TextField("Enter Temperature", value: $inputTemp, formatter: numberFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($tempIsFocused)
+                    
+                    Picker("Temperature", selection: $temperature) {
+                        ForEach(temperatures, id: \.self) { Text($0) }
+                    }
+                }
+                
+                Section {
+                    Picker("Temperature", selection: $outputTemp) {
+                        ForEach(temperatures, id: \.self) { Text($0) }
+                    }
+                    .pickerStyle(.segmented)
+                    Text("Temperature: \(output)")
+                }
+            }
+            .navigationTitle("Temperatures Unit Converter")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if tempIsFocused {
+                    Button("Done") {
+                        tempIsFocused = false
+                    }
+                }
+
             }
         }
     }
 }
+
 #Preview {
-   // ContentView()
-    NavigationView()
+    ContentView()
 }
